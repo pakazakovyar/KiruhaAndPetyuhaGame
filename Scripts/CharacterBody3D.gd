cd46b8520044204777cbd7aef4c732c1a3c28a55
Pudgerus <kirill.iskand@yandex.ru>
2024-02-09 20:12:23 +0300

    animation

--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -9,9 +9,10 @@ var gravity = 30
 var health = 5
 var dam = 1
 @onready var healthbar = $HealthBar
-@onready var mesh = $MeshInstance3D
-@onready var parts = $MeshInstance3D/CPUParticles3D
+@onready var mesh = $hero
+@onready var parts = $hero/CPUParticles3D
 @onready var timer = $Timer
+@onready var anim = $hero/AnimationPlayer
 
 func _ready():
 	health = 5
@@ -37,7 +38,9 @@ func _physics_process(delta):
 		mesh.rotation.y = 190 * delta
 	if direction:
 		velocity.z = direction.z * SPEED
+		anim.play("run")
 	else:
+		anim.play("idle")
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	if Input.is_action_just_pressed("shift"):
 		velocity.z = direction.z * 20 * SPEED
@@ -45,9 +48,11 @@ func _physics_process(delta):
 		parts.gravity.z = direction.z
 	elif Input.is_action_pressed("shift"):
 		velocity.z = direction.z * 2 * SPEED
+		anim.play("fastrun")
 	if Input.is_action_just_released("shift"):
 		parts.emitting = false
 	move_and_slide()
+	
 	
 func _change_health(value):
 	healthbar.health -= value
