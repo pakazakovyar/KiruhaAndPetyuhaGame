c2c97649300afe0c30130244401347d823ea64e2
Pudgerus <kirill.iskand@yandex.ru>
2024-02-16 10:43:19 +0300

    uzhe ne pomnu

--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -26,9 +26,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	
-		
 
 	var input_dir = Input.get_vector("up", "down", "right", "left")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
